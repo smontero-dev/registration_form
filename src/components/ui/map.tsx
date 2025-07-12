e4133176088ee8d35e64f2d6128c2614a180ac17
Santiago Montero <smontero.dev@gmail.com>
2025-07-13 00:42:48 +0200

    v2 - map ready to add markers

--- src/components/ui/map.tsx
+++ src/components/ui/map.tsx
@@ -1,7 +1,50 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function Map() {
+type MapProps = {
+  markers: { lat: number; lng: number }[];
+  onMapClick: (lat: number, lng: number) => void;
+};
+
+type LocationMarkersProps = {
+  markers: { lat: number; lng: number }[];
+};
+
+type OnMapClickProps = {
+  onMapClick: (lat: number, lng: number) => void;
+};
+
+function LocationMarkers({ markers }: LocationMarkersProps) {
+  return (
+    <>
+      {markers &&
+        markers.map((marker, index) => (
+          <Marker key={index} position={[marker.lat, marker.lng]}>
+            <Popup>
+              Marker at: {marker.lat.toFixed(4)}, {marker.lng.toFixed(4)}
+            </Popup>
+          </Marker>
+        ))}
+    </>
+  );
+}
+
+function OnMapClick({ onMapClick }: OnMapClickProps) {
+  useMapEvents({
+    click: (e) => {
+      onMapClick(e.latlng.lat, e.latlng.lng);
+    },
+  });
+  return null;
+}
+
+export default function Map({ markers, onMapClick }: MapProps) {
   return (
     <div className="h-[500px] rounded-lg overflow-hidden border border-gray-300">
       <MapContainer
@@ -13,7 +56,8 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <LocationMarkers activeMarkerType={activeMarkerType} /> */}
+        <LocationMarkers markers={markers} />
+        <OnMapClick onMapClick={onMapClick} />
       </MapContainer>
     </div>
   );
